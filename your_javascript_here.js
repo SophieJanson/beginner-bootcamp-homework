bc1434e66f66c259e74214273bc3a54ddc0db514
Sophie Janson <sophie@sophiejanson.com>
2018-06-01 17:38:44 +0200

    Init

---
+++ your_javascript_here.js
@@ -0,0 +1,22 @@
+// ## Section 1. Creating the hero object
+//
+// Declare the global variable hero and assign it an object. This object has the following keys and values:
+//
+// 1. `name` which is a string (you choose the name)
+// 2. `heroic` which is a boolean
+// 3. `inventory` which is an array
+// 4. `health` which is a number
+// 5. `weapon` which is an object (it has some stats in the form keys and values)
+// 6. `weapon` has a key `type` which is a string
+// 7. `weapon` has a key `damage` which is a number
+//
+// If you've implemented these instructions you console in the browser should read:
+//
+// `Hero tests passed! `
+
+
+// Variables
+
+// Game logic
+
+// UI
